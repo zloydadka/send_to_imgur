0ca296307530c8be0b1c1bdf820a46975c58fa46
Alexandr Andrianov <alexandr.andrianov@rdtex.ru>
2013-09-13 13:48:27 +0400

    link copy to clipdoard

--- send_to_imgur.sh
+++ send_to_imgur.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
+cliend_id="bf89688ea4b3eef"
 
-
-response=$(/usr/bin/env curl -# -X POST -H "Authorization: Client-ID bf89688ea4b3eef" -F "image=@$1" https://api.imgur.com/3/upload )
+response=$(/usr/bin/env curl -# -X POST -H "Authorization: Client-ID $cliend_id" -F "image=@$1" https://api.imgur.com/3/upload )
 link=$(echo $response | sed -r 's/.*"link":"(.*)"}.*/\1/g' | sed 's/\\//g')
+
+qdbus org.kde.klipper /klipper setClipboardContents "$link"
 xdg-open $link
